de7697a48adce2769a1112132409649a817f436a
artemetr <a.a.shirokikh@072.etagi.com>
2022-05-18 18:29:33 +0500

    Добавил абстрактный класс интерпретатора, чтоб можно было использовать в качестве типизации

--- excel2pycl/__init__.py
+++ excel2pycl/__init__.py
@@ -3,6 +3,7 @@ from excel2pycl.src.context import Context
 from excel2pycl.src.excel import Excel, ExcelSafeException
 from excel2pycl.src.object_loader import load_module
 from excel2pycl.src.translators import CellTranslator
+from excel2pycl.src.abstract_excel_in_python_class import AbstractExcelInPython
 
 Cell = Cell
 Context = Context
@@ -10,7 +11,8 @@ Excel = Excel
 ExcelSafeException = ExcelSafeException
 load_module = load_module
 CellTranslator = CellTranslator
+AbstractExcelInPython = AbstractExcelInPython
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 __author__ = 'Esoft'
 __license__ = 'MIT'
---
+++ excel2pycl/src/abstract_excel_in_python_class.py
@@ -0,0 +1,59 @@
+from abc import ABC
+
+
+class AbstractExcelInPython(ABC):
+    def __init__(self, arguments: dict = None):
+        if arguments is None:
+            arguments = {}
+        self._arguments = {}
+        self.set_arguments(arguments)
+
+    def set_arguments(self, arguments: dict):
+        self._arguments = {
+            **self._arguments,
+            **{i['uid']: i['value'] for i in arguments}
+        }
+
+    def _flatten_list(self, subject: list) -> list:
+        result = []
+        for i in subject:
+            if type(i) == list:
+                result = result + self._flatten_list(i)
+            else:
+                result.append(i)
+
+        return result
+
+    @staticmethod
+    def _only_numeric_list(flatten_list: list):
+        return [i for i in flatten_list if type(i) in [float, int]]
+
+    def _sum(self, flatten_list: list):
+        return sum(self._only_numeric_list(flatten_list))
+
+    def _average(self, flatten_list: list):
+        return self._sum(flatten_list) / len(self._only_numeric_list(flatten_list))
+
+    def _vlookup(self, lookup_value, table_array: list, col_index_num: int, range_lookup: bool = False):
+        # TODO add Range Lookup (https://support.microsoft.com/en-us/office/vlookup-function-0bbc8083-26fe-4963-8ab8-93a18ad188a1)
+        # TODO search optimization needed
+        for row in table_array:
+            if row[0] == lookup_value:
+                return row[col_index_num - 1]
+
+        return 1  # When the vlookup has not found anything similar to lookup_value, it returns 1 in the excel implementation for this function
+
+    def _sum_if(self, range_: list, criteria: callable, sum_range: list = None):
+        result = 0
+        range_, sum_range = self._flatten_list(range_), self._flatten_list(sum_range)
+        for i in range(len(range_)):
+            if i < len(sum_range) and criteria(range_[i]):
+                result += sum_range[i] or 0
+
+        return result
+
+    def _cell_preprocessor(self, cell_uid: str):
+        return self._arguments.get(cell_uid, self.__dict__.get(cell_uid, self.__class__.__dict__[cell_uid])(self))
+
+    def exec_function_in(self, cell_uid: str):
+        return self._cell_preprocessor(cell_uid)
