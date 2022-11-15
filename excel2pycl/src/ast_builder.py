222a87d4e9a8d059382ee4a665edd4b8e2db4fc7
d.v.gorshkov <d.v.gorshkov@tmn3.etagi.com>
2022-11-15 09:18:50 +0500

    Добавлен проброс ошибки про построении усеченного ast.

--- excel2pycl/src/ast_builder.py
+++ excel2pycl/src/ast_builder.py
@@ -1,8 +1,12 @@
 from excel2pycl.src.cell import Cell
+from excel2pycl.src.exceptions import E2PyclParserException
 from excel2pycl.src.tokens import EntryPointToken
 
 
 class AstBuilder:
     @classmethod
     def parse(cls, expression: list, in_cell: Cell):
-        return EntryPointToken.get(expression, in_cell)[0]
+        ast = EntryPointToken.get(expression, in_cell)
+        if len(ast[0].value[1].value) != len(expression) - 1:
+            raise E2PyclParserException('Unknown tokens composition', ast[1][0], expression)
+        return ast[0]
