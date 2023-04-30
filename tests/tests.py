d35677e664edacc5efcf08487b4b86c591ef684b
Yakov Waimer <ya.e.vaymer@tmn3.etagi.com>
2023-05-01 00:47:29 +0500

    Добавил юнит-тесты с файликом.
    https://youtrack.esoft.tech/issue/MOTIVATION-1617

---
+++ tests/tests.py
@@ -0,0 +1,79 @@
+import unittest
+import uuid
+import os
+from excel2pycl import Parser, Executor, Cell
+
+
+class TestTokens(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.translation_file_path = f'tests/{uuid.uuid4()}.py'
+        cls.parser = Parser() \
+            .set_excel_file_path('tests/tokens.xlsx') \
+            .enable_safety_check() \
+            .write_translation(cls.translation_file_path)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        # после выполнения всех тестов удаляем файлик питоновский
+        os.remove(cls.translation_file_path)
+
+    def test_round_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 1, 2), Cell('base', 1, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='ROUND token are OK')
+
+    def test_or_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 2, 2), Cell('base', 2, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='OR token are OK')
+
+    def test_and_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 3, 2), Cell('base', 3, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='AND token are OK')
+
+    def test_vlookup_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 4, 2), Cell('base', 4, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='VLOOKUP token are OK')
+
+    def test_if_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 5, 2), Cell('base', 5, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='IF token are OK')
+
+    def test_sum_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 6, 2), Cell('base', 6, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='SUM token are OK')
+
+    def test_sumif_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 7, 2), Cell('base', 7, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='SUMIF token are OK')
+
+    def test_average_token(self):
+        cell_values = Executor() \
+            .set_executed_class(class_file=self.translation_file_path) \
+            .get_cells([Cell('base', 8, 2), Cell('base', 8, 3)])
+
+        self.assertEqual(cell_values[0].value, cell_values[1].value, msg='AVERAGE token are OK')
+
+
+if __name__ == '__main__':
+    unittest.main()
