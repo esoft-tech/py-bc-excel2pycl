a13c1c327992099746fe66af4dfb3db51c7d74ed
Yakov Waimer <yakov.05@mail.ru>
2023-05-02 17:59:46 +0500

    Отвязал от файла, т.к. иначе при мердж реквестах придется постоянно докидывать токены в файл
    https://youtrack.esoft.tech/issue/MOTIVATION-1619

---
+++ test/worksheet_creator.py
@@ -0,0 +1,40 @@
+from openpyxl import Workbook
+from openpyxl.worksheet.table import Table, TableStyleInfo
+from datetime import datetime
+
+def create_test_table(file_name):
+    wb = Workbook()
+    ws = wb.active
+
+    data = [
+        # ROUND - 2 строка
+        ['=ROUND(A4, 2)', '=OR(B4, B5)', '=AND(C4, C5)', '=VLOOKUP(5, D4:D13, 1, FALSE())', '=IF(E6>E5, 44, 11)',
+         '=SUM(F4:F13)', '=SUMIF(G4:G13, ">6")', '=AVERAGE(H4:H13)', '=YEAR(I4)', '=MONTH(J4)', '=DAY(K4)'],
+        [10.24, '=TRUE()', '=FALSE()', 5, 44, 55, 34, 5.5, 2023, 10, 10],
+        [10.239584, '', 0, 1, 1, 1, 1, 1, datetime(2023, 10, 10), datetime(2023, 10, 10), datetime(2023, 10, 10)],
+        ['', 100, 1, 2, 2, 2, 2, 2, '', '', ''],
+        ['', '', '', 3, 3, 3, 3, 3, '', '', ''],
+        ['', '', '', 4, 4, 4, 4, 4, '', '', ''],
+        ['', '', '', 5, 5, 5, 5, 5, '', '', ''],
+        ['', '', '', 6, 6, 6, 6, 6, '', '', ''],
+        ['', '', '', 7, 7, 7, 7, 7, '', '', ''],
+        ['', '', '', 8, 8, 8, 8, 8, '', '', ''],
+        ['', '', '', 9, 9, 9, 9, 9, '', '', ''],
+        ['', '', '', 10, 10, 10, 10, 10, '', '', ''],
+    ]
+
+    # add column headings. NB. these must be strings
+    ws.append(["ROUND", "OR", "AND", "VLOOKUP", "IF", "SUM", "SUMIF", "AVERAGE", "YEAR",
+               "MONTH", "DAY"])
+    for row in data:
+        ws.append(row)
+
+    tab = Table(displayName="base", ref="A1:E5")
+
+    # Add a default style with striped rows and banded columns
+    style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
+                           showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+    tab.tableStyleInfo = style
+
+    ws.add_table(tab)
+    wb.save(file_name)
