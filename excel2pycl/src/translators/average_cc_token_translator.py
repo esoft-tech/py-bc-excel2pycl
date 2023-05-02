e86b16d3436188407969d94ca0b99e2f34087a9c
Yakov Waimer <yakov.05@mail.ru>
2023-05-02 12:31:53 +0500

    fix average token
    https://youtrack.esoft.tech/issue/MOTIVATION-1617

--- excel2pycl/src/translators/average_cc_token_translator.py
+++ excel2pycl/src/translators/average_cc_token_translator.py
@@ -11,6 +11,5 @@ class AverageControlConstructionTokenTranslator(AbstractTranslator):
         flatten_list = context.set_sub_cell(token.in_cell, 'self._flatten_list([' + ','.join(
             [ExpressionTokenTranslator.translate(i, excel, context) for i in token.expressions]) + '])')
         flatten_numeric_list = context.set_sub_cell(token.in_cell, f'self._only_numeric_list({flatten_list})')
-        flatten_numeric_list_sum = context.set_sub_cell(token.in_cell, f'self._sum({flatten_numeric_list})')
 
-        return f'self._average({flatten_numeric_list_sum})'
+        return f'self._average({flatten_numeric_list})'
