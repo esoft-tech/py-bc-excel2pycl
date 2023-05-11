0327c9952e75c2fc28ae688b0f52a205c1f25586
Yakov Waimer <ya.e.vaymer@tmn3.etagi.com>
2023-05-11 18:52:03 +0500

    review fix

--- excel2pycl/src/translators/min_cc_token_translator.py
+++ excel2pycl/src/translators/min_cc_token_translator.py
@@ -2,13 +2,11 @@ from excel2pycl.src.context import Context
 from excel2pycl.src.excel import Excel
 from excel2pycl.src.tokens import SumControlConstructionToken
 from excel2pycl.src.translators.abstract_translator import AbstractTranslator
+from excel2pycl.src.utilities.helper import get_flatten_list
 
 
 class MinControlConstructionTokenTranslator(AbstractTranslator):
     @classmethod
     def translate(cls, token: SumControlConstructionToken, excel: Excel, context: Context) -> str:
-        from excel2pycl.src.translators.expression_token_translator import ExpressionTokenTranslator
-        flatten_list = context.set_sub_cell(token.in_cell, 'self._flatten_list([' + ','.join(
-            [ExpressionTokenTranslator.translate(i, excel, context) for i in token.expressions]) + '])')
-        flatten_numeric_list = context.set_sub_cell(token.in_cell, f'self._only_numeric_list({flatten_list})')
+        flatten_numeric_list = get_flatten_list(token, excel, context)
         return context.set_sub_cell(token.in_cell, f'self._min({flatten_numeric_list})')
