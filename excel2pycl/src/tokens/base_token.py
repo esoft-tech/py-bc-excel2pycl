334d7773aed77db41ece4af299eb284e6a9eed93
Yakov Waimer <yakov.05@mail.ru>
2023-05-25 12:02:16 +0500

    fix subclasses order

--- excel2pycl/src/tokens/base_token.py
+++ excel2pycl/src/tokens/base_token.py
@@ -28,18 +28,28 @@ class BaseToken:
 
         if not cls._SUBCLASSES:
             subclasses = cls.__subclasses__()
-            nested_classes = []
-            for_difference_update = [UndefinedToken]
-            for class_ in subclasses:
-                if class_.__subclasses__():
-                    nested_classes += class_.subclasses()
-                    for_difference_update.append(class_)
+            subclasses_first_rank = cls._remove_subclasses_lower_rank(subclasses)
 
-            subclasses += nested_classes
-            subclasses = set(subclasses)
-            subclasses.difference_update(set(for_difference_update))
-
-            cls._SUBCLASSES = list(subclasses)
+            cls._SUBCLASSES = list(subclasses_first_rank)
             cls._SUBCLASSES.append(UndefinedToken)
 
         return cls._SUBCLASSES
+
+    def _remove_subclasses_lower_rank(cls, subclasses) -> dict:
+        from excel2pycl.src.tokens.undefined_token import UndefinedToken
+
+        nested_classes = []
+        for_difference_update = [UndefinedToken]
+        for class_ in subclasses:
+            if class_.__subclasses__():
+                nested_classes += class_.subclasses()
+                for_difference_update.append(class_)
+
+            subclasses += nested_classes
+
+            subclasses = dict.fromkeys(subclasses)
+            for subclass in for_difference_update:
+                if subclass in subclasses:
+                    del subclasses[subclass]
+
+        return subclasses
