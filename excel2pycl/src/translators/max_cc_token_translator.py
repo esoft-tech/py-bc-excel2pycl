e42aaf730ef361203562239a3d2ccefffde81149
Yakov Waimer <ya.e.vaymer@tmn3.etagi.com>
2023-05-29 17:38:56 +0500

    fix min max typing

--- excel2pycl/src/translators/max_cc_token_translator.py
+++ excel2pycl/src/translators/max_cc_token_translator.py
@@ -1,12 +1,12 @@
 from excel2pycl.src.context import Context
 from excel2pycl.src.excel import Excel
-from excel2pycl.src.tokens import SumControlConstructionToken
+from excel2pycl.src.tokens import MaxControlConstructionToken
 from excel2pycl.src.translators.abstract_translator import AbstractTranslator
 from excel2pycl.src.utilities.helper import get_flatten_numeric_list
 
 
 class MaxControlConstructionTokenTranslator(AbstractTranslator):
     @classmethod
-    def translate(cls, token: SumControlConstructionToken, excel: Excel, context: Context) -> str:
+    def translate(cls, token: MaxControlConstructionToken, excel: Excel, context: Context) -> str:
         flatten_numeric_list = get_flatten_numeric_list(token, excel, context)
         return context.set_sub_cell(token.in_cell, f'self._max({flatten_numeric_list})')
--- excel2pycl/src/translators/min_cc_token_translator.py
+++ excel2pycl/src/translators/min_cc_token_translator.py
@@ -1,12 +1,12 @@
 from excel2pycl.src.context import Context
 from excel2pycl.src.excel import Excel
-from excel2pycl.src.tokens import SumControlConstructionToken
+from excel2pycl.src.tokens import MinControlConstructionToken
 from excel2pycl.src.translators.abstract_translator import AbstractTranslator
 from excel2pycl.src.utilities.helper import get_flatten_list
 
 
 class MinControlConstructionTokenTranslator(AbstractTranslator):
     @classmethod
-    def translate(cls, token: SumControlConstructionToken, excel: Excel, context: Context) -> str:
+    def translate(cls, token: MinControlConstructionToken, excel: Excel, context: Context) -> str:
         flatten_numeric_list = get_flatten_list(token, excel, context)
         return context.set_sub_cell(token.in_cell, f'self._min({flatten_numeric_list})')
