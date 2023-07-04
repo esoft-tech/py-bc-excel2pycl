f5363f7c9e512f2cd75f4b9224e213951b07643c
Yakov Waimer <yakov.05@mail.ru>
2023-07-04 15:03:10 +0500

    fix sequence of regexp tokens

--- excel2pycl/src/tokens/regexp_tokens/__init__.py
+++ excel2pycl/src/tokens/regexp_tokens/__init__.py
@@ -91,6 +91,10 @@ class RoundKeywordToken(RegexpBaseToken):
     regexp = r'ROUND'
 
 
+class MatchKeywordToken(RegexpBaseToken):
+    regexp = r'MATCH'
+
+
 class DateKeywordToken(RegexpBaseToken):
     regexp = r'DATE'
 
@@ -147,33 +151,41 @@ class EKeywordToken(RegexpBaseToken):
     regexp = r'E'
 
 
+class XKeywordToken(RegexpBaseToken):
+    regexp = r'X'
+
+
+class SKeywordToken(RegexpBaseToken):
+    regexp = r'S'
+
+
 # TODO добавить условие для локализации
 class SeparatorToken(RegexpBaseToken):
     regexp = r';|,'
-
-
-class EqOperatorToken(RegexpBaseToken):
-    regexp = r'='
 
 
 class NotEqOperatorToken(RegexpBaseToken):
     regexp = r'<>'
 
 
-class GtOperatorToken(RegexpBaseToken):
-    regexp = r'>'
+class EqOperatorToken(RegexpBaseToken):
+    regexp = r'='
 
 
 class GtOrEqualOperatorToken(RegexpBaseToken):
     regexp = r'>='
 
 
-class LtOperatorToken(RegexpBaseToken):
-    regexp = r'<'
-
-
 class LtOrEqualOperatorToken(RegexpBaseToken):
     regexp = r'<='
+
+
+class GtOperatorToken(RegexpBaseToken):
+    regexp = r'>'
+
+
+class LtOperatorToken(RegexpBaseToken):
+    regexp = r'<'
 
 
 class PlusOperatorToken(RegexpBaseToken):
@@ -234,15 +246,3 @@ class BracketFinishToken(RegexpBaseToken):
 
 class WhitespaceToken(RegexpBaseToken):
     regexp = r'[\s\n\t]+'
-
-
-class XKeywordToken(RegexpBaseToken):
-    regexp = r'X'
-
-
-class MatchKeywordToken(RegexpBaseToken):
-    regexp = r'MATCH'
-
-
-class SKeywordToken(RegexpBaseToken):
-    regexp = r'S'
