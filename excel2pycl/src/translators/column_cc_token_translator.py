040e87c7f2ee45cb1b7d991f76232bd4bad49c30
ansh <anshfreelance1@gmail.com>
2023-08-04 15:47:40 +0500

    solved matrix test

--- excel2pycl/src/translators/column_cc_token_translator.py
+++ excel2pycl/src/translators/column_cc_token_translator.py
@@ -1,3 +1,5 @@
+from openpyxl.utils import column_index_from_string
+
 from excel2pycl.src.context import Context
 from excel2pycl.src.excel import Excel
 from excel2pycl.src.tokens import ColumnControlConstructionToken
@@ -7,9 +9,22 @@ from excel2pycl.src.translators.abstract_translator import AbstractTranslator
 class ColumnControlConstructionTokenTranslator(AbstractTranslator):
     @classmethod
     def translate(cls, token: ColumnControlConstructionToken, excel: Excel, context: Context) -> str:
+        from excel2pycl.src.translators.matrix_of_cell_identifiers_token_translator import \
+            MatrixOfCellIdentifiersTokenTranslator
+
         if token.cell:
-            return str(token.cell.cell.column + 1)
+            return str(column_index_from_string(token.cell.cell.column))
         elif token.matrix:
-            return str(list(set([str(cell.column + 1) for cell in token.matrix.matrix])))
+            # Mutates matrix, inplace literal cols with digital
+            MatrixOfCellIdentifiersTokenTranslator.translate(token.matrix, excel, context)
+
+            if token.matrix.matrix[0].column == token.matrix.matrix[-1].column:
+                return str(token.matrix.matrix[0].column + 1)
+
+            for i in range(token.matrix.matrix[0].column + 1, token.matrix.matrix[-1].column + 2):
+                # Единственный способ, которым получилось установить несколько значений в разные ячейки при
+                # парсинге одного токена
+                context.set_cell(token.in_cell, str(i))
+                token.in_cell.column += 1
         else:
             return token.in_cell.column + 1
